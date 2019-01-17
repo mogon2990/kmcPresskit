6d4b0a132968fd1780547ce48bc475c3700e6801
Monica Gonzalez <mogon2990@gmail.com>
2019-01-16 22:47:00 -0500

    refactor screen size logic for navbar

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -15,77 +15,74 @@ const PATHS = [{path: '/history', text: 'Company History'},
                {path: '/connect', text: 'Connect'}]
 
 export default class Navbar extends Component {
-  constructor() {
-      super()
-      this.state = {
-        isSmallScreen: true,
-        isOpen: false,
-      }
-      this.toggleNavbar = this.toggleNavbar.bind(this)
-      this.handleOutsideClick = this.handleOutsideClick.bind(this)
-  }
-
-  componentDidMount() {
-    window.outerWidth < 800
-    ? this.setState( { isSmallScreen: true} )
-    : this.setState( { isSmallScreen: false, isOpen: true })
-  }
-
-  toggleNavbar() {
-    if (this.state.isOpen) {
-      document.removeEventListener('click', this.handleOutsideClick)
-    } else {
-      document.addEventListener('click', this.handleOutsideClick)
+    constructor() {
+        super()
+        this.state = {
+          isOpen: false,
+        }
+        this.handleResize = this.handleResize.bind(this)
+        this.toggleNavbar = this.toggleNavbar.bind(this)
+        this.handleOutsideClick = this.handleOutsideClick.bind(this)
     }
-    this.setState({ isOpen: !this.state.isOpen})
-  }
 
-  handleOutsideClick(e) {
-    e.preventDefault()
-    if (this.node.contains(e.target)) {
-      return
+    componentDidMount() {
+        window.outerWidth > 800 && this.setState({ isOpen: true })
+        window.addEventListener("resize", this.updateDimensions)
     }
-      this.toggleNavbar()
 
-  }
+    handleResize() {
+        window.innerWidth > 801 && this.setState({ isOpen: true })
+    }
 
-  render() {
-    let { isSmallScreen, isOpen } = this.state
-    let { toggleNavbar } = this
-    return (
-      <div className='navbar-container'>
-          {isSmallScreen &&
-              <div className='sm-nav-top'>
+    toggleNavbar() {
+        if (this.state.isOpen) {
+          document.removeEventListener('click', this.handleOutsideClick)
+        } else {
+          document.addEventListener('click', this.handleOutsideClick)
+        }
+        this.setState({ isOpen: !this.state.isOpen})
+    }
+
+    handleOutsideClick(e) {
+        e.preventDefault()
+        if (this.node.contains(e.target)) {
+          return
+        }
+          this.toggleNavbar()
+    }
+
+    render() {
+        let { isOpen } = this.state
+        let { toggleNavbar } = this
+        return (
+            <div className='navbar-container'>
+                <div className='sm-nav-top'>
                     <ReactSVG
-                      svgClassName='menu-icon'
-                      src={isOpen ? './letter-x-light.svg' : './menu-light.svg'}
-                      onClick={toggleNavbar}/>
-                      <h1 className='sm-nav-title'>Kyle Marshall Choreography</h1>
-              </div>
-          }
-          <CSSTransition
-              in={this.state.isOpen}
-              timeout={150}
-              classNames='nav-links-container'
-              mountOnEnter
-              unmountOnExit
-              appear={true}
-              >
-              <div className='nav-links-container'  ref={node => { this.node = node }}>
-                  {!isSmallScreen  &&
-                      <h1 className='lg-nav-title'>Kyle Marshall Choreography</h1>
-                  }
-                  {PATHS.map(info => {
-                    return <NavLink className='nav-link'
-                                    activeClassName='active'
-                                    onClick={toggleNavbar}
-                                    to={info.path}
-                                    key={info.path}>{info.text}</NavLink>
-                  })}
-              </div>
-              {/* </div> */}
-          </CSSTransition>
-      </div>
-    )
-  }
+                        svgClassName='menu-icon'
+                        src={isOpen ? './letter-x-light.svg' : './menu-light.svg'}
+                        onClick={toggleNavbar}/>
+                    <h1 className='sm-nav-title'>Kyle Marshall Choreography</h1>
+                </div>
+                <CSSTransition
+                    in={this.state.isOpen}
+                    timeout={150}
+                    classNames='nav-links-container'
+                    mountOnEnter
+                    unmountOnExit
+                    appear={true}
+                    >
+                    <div className='nav-links-container'  ref={node => { this.node = node }}>
+                        <h1 className='lg-nav-title'>Kyle Marshall Choreography</h1>
+                        {PATHS.map(info => {
+                          return <NavLink className='nav-link'
+                                          activeClassName='active'
+                                          onClick={toggleNavbar}
+                                          to={info.path}
+                                          key={info.path}>{info.text}</NavLink>
+                        })}
+                    </div>
+                </CSSTransition>
+            </div>
+        )
+    }
 }
